6da86427b3bcf70c39e31b85de6141a67fb6c3b5
kyosuke <cebake1129@gmail.com>
2015-11-05 11:22:23 +0900

    my page

--- baseball/app/models/user.rb
+++ baseball/app/models/user.rb
@@ -1,6 +1,40 @@
 class User < ActiveRecord::Base
+  belongs_to :team
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, #, :confirmable,
+         authentication_keys: [:email, :team_key]
+
+  attr_accessor :team_key
+
+  before_validation :team_key_to_id, if: :has_team_key?
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    team_key = conditions.delete(:team_key)
+    team_id = Team.where(key: team_key).first
+    email = conditions.delete(:email)
+
+    if team_id && email
+      where(conditions).where(["team_id = :team_id AND email = :email",
+        { team_id: team_id, email: email }]).first
+    elsif conditions.has_key?(:confirmation_token)
+      where(conditions).first
+    else
+      false
+    end
+  end
+
+  private
+  def has_team_key?
+    team_key.present?
+  end
+
+  def team_key_to_id
+    team = Team.where(key: team_key).first_or_create
+    self.team_id = team.id
+  end
+
 end
--- baseball/db/schema.rb
+++ baseball/db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104080508) do
+ActiveRecord::Schema.define(version: 20151105021629) do
 
-  create_table "uers", force: :cascade do |t|
+  create_table "teams", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.text     "detail",     limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.integer  "team_id",                limit: 4
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,15 +32,12 @@ ActiveRecord::Schema.define(version: 20151104080508) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
-  add_index "uers", ["email"], name: "index_uers_on_email", unique: true, using: :btree
-  add_index "uers", ["reset_password_token"], name: "index_uers_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
