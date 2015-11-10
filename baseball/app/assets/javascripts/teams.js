e8d5e6e87362cbf26ea757ea3d65a0d8ddd44592
kyosuke <cebake1129@gmail.com>
2015-11-10 16:49:05 +0900

    非表示設定

---
+++ baseball/app/assets/javascripts/teams.js
@@ -0,0 +1,26 @@
+
+
+window.addEventListener("load", function() {
+  $('#check').click(function() {
+    $(".average").slideToggle(this.checked);
+  });
+});
+
+
+window.addEventListener("load", function() {
+  $('#check2').click(function() {
+    $(".on_base").slideToggle(this.checked);
+  });
+});
+
+window.addEventListener("load", function() {
+  $('#check3').click(function() {
+    $(".sulluging").slideToggle(this.checked);
+  });
+});
+
+window.addEventListener("load", function() {
+  $('#check4').click(function() {
+    $(".ops").slideToggle(this.checked);
+  });
+});
